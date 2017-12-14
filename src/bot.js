a442c219f7fcc5c6caad121bdb9cf76e008b08b6
Taras Novak <taras.novak@randomfractals.com>
2017-12-14 11:34:36 -0600

    added basic search and RT logic

--- src/bot.js
+++ src/bot.js
@@ -1,9 +1,9 @@
 const Twit = require('twit')
 const config = require('./config')
 
-const bot = new Twit(config)
+const Twitter = new Twit(config)
 
-bot.get('followers/list', {
+Twitter.get('followers/list', {
   screen_name: 'CyberDash',
   count: 20
 }, (err, data, response) => {
@@ -16,3 +16,45 @@ bot.get('followers/list', {
     })
   }
 })
+
+
+var retweet = function() {
+  // RT search params
+  // see: https://dev.twitter.com/rest/reference/get/search/tweets
+  var params = {
+    q: '#cyberSec, #hacking',
+    count: 5,
+    result_type: 'recent',
+    lang: 'en'    
+  }
+
+  Twitter.get('search/tweets', params, function(err, data) {
+    // if there no errors
+      if (!err) {
+          // process matching tweets
+          data.statuses.forEach(status => {
+            console.log(status.text, status.user.screen_name)
+            // retweet
+            Twitter.post('statuses/retweet/:id', {id: status.id_str}, function(err, response) {
+              if (response) {
+                console.log('.');
+              }
+              // if there was an error while tweeting
+              if (err) {
+                coneole.error('failed to RT', status)
+              }
+            });
+          })
+      }
+      // if unable to Search a tweet
+      else {
+        console.error('Twitter search failed!');
+      }
+  });
+}
+
+// grab & retweet as soon as program is running...
+retweet();
+
+// retweet in every 50 minutes
+setInterval(retweet, 3000000);
