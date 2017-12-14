d47f02c54bc9a13480d5a82bda950a6426fd3970
Taras Novak <taras.novak@randomfractals.com>
2017-12-14 10:59:53 -0600

    added simple followers list test

--- src/bot.js
+++ src/bot.js
@@ -2,3 +2,17 @@ const Twit = require('twit')
 const config = require('./config')
 
 const bot = new Twit(config)
+
+bot.get('followers/list', {
+  screen_name: 'CyberDash',
+  count: 20
+}, (err, data, response) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('Followers:')
+    data.users.forEach(user => {
+      console.log(user.screen_name)
+    })
+  }
+})
