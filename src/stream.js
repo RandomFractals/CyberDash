ac7866fba99c558f369fbc6aa0f3402e1a6a22d0
Taras Novak <taras.novak@randomfractals.com>
2017-12-17 09:27:40 -0600

    added user friends count log for debug

--- src/stream.js
+++ src/stream.js
@@ -155,7 +155,11 @@ function getKeywordMatches(tweetText) {
  */
 function logTweet (tweet, tweetText, keywords) {
   console.log(`\n${tweet.user.screen_name}: ${tweetText}`)
-  console.log(`followers: ${tweet.user.followers_count} | tweets: ${tweet.user.statuses_count}`)  
+  console.log(
+    `followers: ${tweet.user.followers_count}`,
+    `| friends: ${tweet.user.friends_count}`    
+    `| tweets: ${tweet.user.statuses_count}`
+  )
   console.log('matches:', keywords)
   console.log('hashtags:\n', tweet.entities.hashtags)
   //console.log(tweet)
