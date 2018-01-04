877baacc32aba6358d298062d5270f0b9aadd88c
Taras Novak <taras.novak@randomfractals.com>
2018-01-04 09:32:27 -0600

    seeting webpack score to 5 still yields the best rate quoted tweets results
    added reactjs into the mix as well

--- src/bot.js
+++ src/bot.js
@@ -326,7 +326,9 @@ TwitterBot.prototype.getKeywordMatches = function (text, keywords) {
 TwitterBot.prototype.getSentiment = function (tweet) {
   let tweetSentiment = sentiment(tweet.fullText, {
     // TODO: use track filter keywords from config here and boost all of them?
-    'webpack': -5 // set 'webpack' word sentiment to what it is
+    // boost webpack and reactjs words for better tweet sentiment ratings
+    'webpack': 5,
+    'reactjs': 5
   })
 
   // create tweet rating info
